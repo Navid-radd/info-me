f5ecb66ac464ccc53e6915ecddf3067fd2ba3f36
navid rad <navid_pormanesh@yahoo.com>
2025-05-22 19:14:40 +0330

    v-deploytest

--- info.py
+++ info.py
@@ -102,8 +102,30 @@ logger = logging.getLogger(__name__)
 # اتصال به Google Sheets
 try:
     logger.info("در حال اتصال به Google Sheets...")
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "radoteam-0da92609cd4a.json", scope)
+    
+    # تعریف scope
+try:
+    logger.info("در حال اتصال به Google Sheets...")
+    
+    # تعریف scope
+    scope = ["https://spreadsheets.google.com/feeds", 
+             "https://www.googleapis.com/auth/drive"]
+    
+    # ایجاد اعتبارنامه از متغیرهای محیطی
+    creds = ServiceAccountCredentials.from_json_keyfile_dict({
+        "type": "service_account",
+        "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+        "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
+        "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+        "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL")
+    }, scope)
+    
+    # احراز هویت
     client = gspread.authorize(creds)
     
     logger.info("در حال باز کردن اسپردشیت...")
